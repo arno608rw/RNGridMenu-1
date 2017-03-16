2d99c89aa3493cc60925f5ad4e38063b1144f6e2
Dmitry Frishbuter <dmitry.frishbuter@rosberry.com>
2017-03-16 10:10:35 +0600

    Add framework target, fix warnings and build issues

---
+++ GridMenu/GridMenu.h
@@ -0,0 +1,19 @@
+//
+//  GridMenu.h
+//  GridMenu
+//
+//  Created by Dmitry Frishbuter on 16/03/2017.
+//  Copyright © 2017 Ryan Nystrom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GridMenu.
+FOUNDATION_EXPORT double GridMenuVersionNumber;
+
+//! Project version string for GridMenu.
+FOUNDATION_EXPORT const unsigned char GridMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GridMenu/PublicHeader.h>
+
+
